ce366d29255572e26ea3b8e5cd5512e0f870babe
Sanjay kumar <101125212+sanjayamli@users.noreply.github.com>
2023-08-02 21:14:35 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -2,9 +2,9 @@ let p = fetch(
 'https://goweather.herokuapp.com/weather/Curitiba');
 p.then((value1)=>{
      
-      console.log(value1.status)
-      console.log(value1.ok)
+      //console.log(value1.status)
+     // console.log(value1.ok)
       return value1.json()
     }).then((value2)=>{
-    console.log( value2)
+    return  value2
 })
